169d3236585cde8d04d34d918228f88ec7f8c708
Liza <rummo_liza@mail.ru>
2021-07-05 13:36:00 +0300

    first activity

--- app/src/main/java/com/example/deadspace/view/ui/main/StartFragment.kt
+++ app/src/main/java/com/example/deadspace/view/ui/main/StartFragment.kt
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.deadspace.view.R
+import com.example.deadspace.R
 
 class StartFragment : Fragment() {
 
