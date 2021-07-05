b96293ea5a1c3dcd7af3673fc135be00b4b6db6e
Liza <rummo_liza@mail.ru>
2021-07-05 13:30:53 +0300

    first activity

--- app/src/main/java/com/example/deadspace/view/StartActivity.kt
+++ app/src/main/java/com/example/deadspace/view/StartActivity.kt
@@ -2,6 +2,7 @@ package com.example.deadspace.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.deadspace.R
 import com.example.deadspace.view.ui.main.StartFragment
 
 class StartActivity : AppCompatActivity() {
