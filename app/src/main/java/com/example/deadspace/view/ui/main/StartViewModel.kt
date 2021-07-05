b14021531b46b4b72d6717226274d305e99f77f6
Liza <rummo_liza@mail.ru>
2021-07-05 13:29:43 +0300

    first activity

---
+++ app/src/main/java/com/example/deadspace/view/ui/main/StartViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.deadspace.view.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class StartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/deadspace/view/view.kt
+++
@@ -1,4 +0,0 @@
-package com.example.deadspace.view
-
-one
-
