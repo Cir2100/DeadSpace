0376b439c0fb6dc729996d54f744b9fae43e20a5
Sergey <kurilov.sereja2012@yandex.ru>
2021-07-06 11:32:53 +0300

    i'm smart

---
+++ app/src/main/java/com/example/deadspace/view/StartActivity.kt
@@ -0,0 +1,19 @@
+package com.example.deadspace.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deadspace.R
+import com.example.deadspace.view.ui.main.StartFragment
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, StartFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/deadspace/view/ui/main/StartFragment.kt
@@ -0,0 +1,35 @@
+package com.example.deadspace.view.ui.main
+
+import android.arch.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.deadspace.R
+
+class StartFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = StartFragment()
+    }
+
+    private lateinit var viewModel: StartViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.start_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(StartViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
