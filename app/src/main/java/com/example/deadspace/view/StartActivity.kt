57d700fc42ef85a7c0d1ed5f13baf09d2a6069f8
Liza <rummo_liza@mail.ru>
2021-07-07 14:49:57 +0300

    first day _3

--- app/src/main/java/com/example/deadspace/view/StartActivity.kt
+++ app/src/main/java/com/example/deadspace/view/StartActivity.kt
@@ -3,18 +3,12 @@ package com.example.deadspace.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.deadspace.R
-import com.example.deadspace.schedule.sheadule
-import com.example.deadspace.view.ui.main.StartFragment
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, StartFragment.newInstance())
-                .commitNow()
-        }
+
     }
 }
