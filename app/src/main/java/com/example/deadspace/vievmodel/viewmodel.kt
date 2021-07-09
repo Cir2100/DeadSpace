44d1ad0306b10bc7ef93c87b2532196ef486d748
Sergey <kurilov.sereja2012@yandex.ru>
2021-07-09 14:08:23 +0300

    add binging

--- app/src/main/java/com/example/deadspace/vievmodel/viewmodel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.deadspace.vievmodel
-
-class viewmodel {
-}
--- app/src/main/java/com/example/deadspace/view/ui/main/StartViewModel.kt
+++ app/src/main/java/com/example/deadspace/view/ui/main/StartViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.deadspace.view.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class StartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class StartViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
+    val s : LiveData<String> = TODO()
 }
