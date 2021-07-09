854889c90447a329f2d4e264369998afe531f715
Sergey <kurilov.sereja2012@yandex.ru>
2021-07-09 14:32:14 +0300

    add binging

--- app/src/main/java/com/example/deadspace/view/StartActivity.kt
+++ app/src/main/java/com/example/deadspace/view/StartActivity.kt
@@ -2,6 +2,9 @@ package com.example.deadspace.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.example.deadspace.R
 import com.example.deadspace.schedule.SheaduleLodaer
 import com.example.deadspace.view.ui.main.StartFragment
@@ -11,11 +14,7 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, StartFragment.newInstance())
-                .commitNow()
-        }
+        //val binding : ViewDataBinding = DataBindingUtil.setContentView(this, R.layout.start_activity)
         //TODO: use normal
         SheaduleLodaer().loadSheadule("1942", true)
     }
--- app/src/main/java/com/example/deadspace/view/ui/main/StartFragment.kt
+++ app/src/main/java/com/example/deadspace/view/ui/main/StartFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.deadspace.R
@@ -15,13 +17,6 @@ class StartFragment : Fragment() {
     }
 
     private lateinit var viewModel: StartViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.start_fragment, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
