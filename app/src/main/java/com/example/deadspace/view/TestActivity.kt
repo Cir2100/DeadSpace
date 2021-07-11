dbf80ea05b9b3ee3eebbff9ba6ece473ab0002c3
ValeriaShiryaeva <shiryayeva.valeria.18@mail.ru>
2021-07-11 15:38:35 +0300

    day 5 (4 экрана, убран заголовок на главной странице)

---
+++ app/src/main/java/com/example/deadspace/view/TestActivity.kt
@@ -0,0 +1,19 @@
+package com.example.deadspace.view
+
+import com.example.deadspace.R
+import android.content.Intent
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+
+class TestActivityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deadlines)
+
+    }
+}
