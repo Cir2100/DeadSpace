5fe7cbf57d4c083657546b864977d050b52f8548
Liza <rummo_liza@mail.ru>
2021-07-12 15:08:46 +0300

    day 6 (6/7 экранов, сделаны элементы списков, добавлено расписание)

---
+++ app/src/main/java/com/example/deadspace/view/RaspActivity.kt
@@ -0,0 +1,12 @@
+package com.example.deadspace.view
+
+import com.example.deadspace.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RaspActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rasp)
+    }
+}
