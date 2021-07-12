ab2875dcf55ea824a4d999d3149f52f2de4f50e1
Liza <rummo_liza@mail.ru>
2021-07-12 17:30:01 +0300

    day 6 (7/7 экранов, создана кнопка добавления занятий, не работает)

---
+++ app/src/main/java/com/example/deadspace/view/AddRaspActivity.kt
@@ -0,0 +1,20 @@
+package com.example.deadspace.view
+
+import com.example.deadspace.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.content.Intent
+
+class AddRaspActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_rasp)
+    }
+
+    fun ClickBackAddRasp(view:View)
+    {
+        val BackAddRaspIntent = Intent(this, RaspActivity::class.java)
+        startActivity(BackAddRaspIntent)
+    }
+}
